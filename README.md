0f43a5492adff2f03cc58742e75e74ebf1ba7262
barrett-shea <barrettkshea@gmail.com>
2021-04-21 11:09:07 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# jammming
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/jammming)
---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import TrackList from '../TrackList/TrackList'
+import './Playlist.css'
+
+class Playlist extends React.Component {
+  render () {
+    return (
+      <div class="Playlist">
+        <input defaultValue={"New Playlist"}/>
+        <TrackList 
+          tracks={this.props.playlistTracks}
+          onRemove= {this.props.onRemove}
+          isRemoval= {true}/>
+        <button class="Playlist-save">
+          SAVE TO SPOTIFY
+        </button>
+      </div>
+    )
+  }
+
+}
+
+export default Playlist;
