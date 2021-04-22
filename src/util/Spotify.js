1ee24c0377e96ae30a580440c936f7d3513862fd
barrett-shea <barrettkshea@gmail.com>
2021-04-21 22:24:00 -0400

    Redirect uri update

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,5 +1,5 @@
 const clientId = 'de7455d403fe4331a6ae2a32f3445299'
-const redirectUri = 'https://jammming-ed61d.firebaseapp.com/'
+const redirectUri = 'https://react-678ef.firebaseapp.com'
 let accessToken;
 
 const Spotify = {
