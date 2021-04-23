f71b87909373ddd6cdcd18db3b8acb274268b248
barrett-shea <barrettkshea@gmail.com>
2021-04-23 12:22:51 -0400

    1 file modified

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -53,7 +53,6 @@ class App extends React.Component {
   }
 
   search (term) {
-    console.log(term);
     Spotify.search(term).then(searchResults => {
       this.setState({searchResults: searchResults})
     })
