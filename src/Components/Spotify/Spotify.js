ef4ac631385c1a168c99aae9a3d8a613fbef882d
barrett-shea <barrettkshea@gmail.com>
2021-04-23 12:56:16 -0400

    1 file modified

--- src/Components/Spotify/Spotify.js
+++ src/Components/Spotify/Spotify.js
@@ -25,8 +25,6 @@ const Spotify = {
   },
   search(term) {
     const accessToken = Spotify.getAccessToken();
-    console.log(term);
-    console.log(accessToken);
     return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
       headers: {
         Authorization: `Bearer ${accessToken}`
