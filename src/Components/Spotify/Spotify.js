a76b153777d274ad04d15b7b4494f359ee8694c5
barrett-shea <barrettkshea@gmail.com>
2021-04-23 12:37:26 -0400

    1 file modified

--- src/Components/Spotify/Spotify.js
+++ src/Components/Spotify/Spotify.js
@@ -16,7 +16,7 @@ const Spotify = {
       accessToken = accessTokenMatch[1];
       const expiresIn = Number(expiresInMatch[1])
 // This clears paramenters, allows to grab new access token when it expires 
-    window.setTimeout(() => accessToken = '', expiresIn * 1000);
+    window.setTimeout(() => accessToken = '', expiresIn * 10000);
     window.history.pushState('Access Token', null, '/');
     } else {
       const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`
